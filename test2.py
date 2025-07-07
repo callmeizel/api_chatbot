e0d3af1e26d5f43d1b5b4aae2b2339c239e08d3a
Gaurav <gauravthapa2727@gmail.com>
2025-07-07 14:00:19 +0530

    second commit

--- test2.py
+++ test2.py
@@ -1,29 +1,36 @@
 from openai import OpenAI, AuthenticationError,APIError
 
-# api-key - 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
+# api-key - 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
 
 client = OpenAI(
-        api_key = "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",
+        api_key = "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",
         base_url = "https://openrouter.ai/api/v1"
 )
 
-prompt = 'explain why <Oxygen> is important? <limit the response under the <<max_tokens>> variable value >'
 
-stream = client.chat.completions.create(
-            model = "deepseek/deepseek-r1-0528",
-            messages = [
-                {
-                    'role' : 'user',
-                    'content' : prompt
-                }
-            ],
-            temperature = 0.1,
-            max_tokens = 500,
-            stream = True
-)
+prompt = 'explain why <Oxygen> is important?'
 
-for chunks in stream:
-    if chunks.choices[0].delta.content:
-        print(chunks.choices[0].delta.content, end='', flush=True)
+try:
+    response = client.chat.completions.create(
+                model = "deepseek/deepseek-r1-0528:free",
+                messages = [
+                    {
+                        'role' : 'user',
+                        'content' : prompt
+                    }
+                ],
+                temperature = 0.1,
+                max_tokens = 500,
+                stream = True
+    )
 
-#print(response.choices[0].message.content)
+    for chunks in response:
+        if chunks.choices[0].delta.content:
+            print(chunks.choices[0].delta.content, end='', flush=True)
+            
+except AuthenticationError:
+    print('Authentication error --check your api key')
+except APIError as apier:
+    print(f"API error : {apier}")
+except Exception as exp:
+    print(f"Error : {exp}")
